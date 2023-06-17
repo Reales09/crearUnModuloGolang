fd13ac3d50c51bf9e630e10079b19598713dac4d
Reales <rclarence123@gmail.com>
2023-06-17 00:20:24 -0500

    Se agrega manejador de errores

--- greetings.go
+++ greetings.go
@@ -1,12 +1,18 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
 )
 
 // Hello devuelve un saludo para la persona especificada
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("Nombre vacio")
+	}
+
 	message := fmt.Sprintf("¡Hola, %v! ¡Bienvenido!", name)
 
-	return message
+	return message, nil
 }
